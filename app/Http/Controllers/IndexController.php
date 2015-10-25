4beff345c4124797f55d20ed965a6ff99ddadb89
Constantine <nawataster@gmail.com>
2015-10-25 08:17:45 +0200

    Init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers;
+
+
+class IndexController extends Controller{
+// class IndexController extends Controller{
+
+    public function getIndex(){
+    	return view( 'index' );
+    }
+//______________________________________________________________________________
+
+}//	Class end
