3b0ad59d3af0a25069044a87a83ca932589cbf0a
Constantine <nawataster@gmail.com>
2015-10-28 22:36:43 +0200

    Draft presentation in full.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,44 +1,12 @@
-@include('blocks/header')
+@extends('layouts.main')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@section('content')
 <div class="panel panel-info main-panel-content">
 	<div class="panel-heading index-heard">
 
 
 <table class="main-tbl">
 	<tr>
-
-
 		<td><p class="faucet-id-p" style="float: right;">Id: <span class="badge">42</span></p></td>
 
 		<td id="act_after_td" class="input-control-td hided">Show after
@@ -46,11 +14,20 @@
 			<input type="hidden" id="oduraion" name="oduraion" />
 		</td>
 
+		<td id="priority_td" class="input-control-td">Priority&nbsp;
+			<input type="text" id="priority" name="priority" class="time-inp" />
+		</td>
+
+		<td class="num-info-td">
+			Faucets (all / active): <span id="n_all_span"></span> / <span id="n_active_span"></span>
+			<span id="last_payment" class="last-payment"></span>
+		</td>
+
 		<td>
 			<div class="btn-group" role="group">
-				<button onclick="getNextFaucet();" type="button" class="btn btn-default btn-sm">Next</button>
-				<button type="button" class="btn btn-default btn-sm">Load</button>
 				<button type="button" class="btn btn-default btn-sm">Settins</button>
+				<button type="button" class="btn btn-default btn-sm">Load</button>
+				<button type="button" class="btn btn-default btn-sm" onclick="getNextFaucet();">Next</button>
 			</div>
 		</td>
 
@@ -64,30 +41,4 @@
 	   <iframe id="main_fraim" class="main-fraim" src="http://rollingfaucet.com/"></iframe>
 	</div>
 </div>
-
-
-
-<?php /* ?>
-
-<div class="panel panel-info main-panel-content">
-<div class="panel-heading index-heard">
-
-
-		<div class="btn-group navbar-left" role="group" aria-label="...">
-			<button onclick="getNextFaucet();" type="button" class="btn btn-default btn-sm">Next</button>
-			<button type="button" class="btn btn-default btn-sm">Load</button>
-			<button type="button" class="btn btn-default btn-sm">Settins</button>
-		</div>
-
-		<ul class="nav navbar-nav navbar-left">
-			<li><h4>Id: <span class="badge">42</span></h4></li>
-			<li>HHHHHH</li>
-			<li>HHHHHH</li>
-		</ul>
-
-</div>
-</div>
-
-<?php */ ?>
-
-@include('blocks/footer')
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Main layout
+ */
+?>
+@include('blocks/header')
+
+@yield('content')
+
+@include('blocks/footer')
